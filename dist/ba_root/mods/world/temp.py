a244c577cb7c8c564e40a220561d8ef2f0e00e5c
LIRIK <74240325+LIRIK-SPENCER@users.noreply.github.com>
2021-07-20 18:55:34 +0530

    v1.0.4

--- dist/ba_root/mods/world/temp.py
+++ dist/ba_root/mods/world/temp.py
@@ -1,13 +1,25 @@
-""" 
-TEMPORARY STORAGE FOR VARIOUS FUNCTIONALITY
-WARNING: DO NOT CHANGE ANYTHING FROM HERE
-"""
+""" TEMPORARY STORAGE FOR VARIOUS FUNCTIONALITY
+# WARNING: DO NOT CHANGE ANYTHING FROM HERE"""
 
-configs = {
-    "last": {},
-    "muted_ids": [],
-    "chatbot_ids": [],
-    "modified_time": {},
-    "nightmode": False,
-    "mute": False,
-}
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    last: dict
+    mute: bool
+    voter: list
+    voters: list
+    muted_ids: list
+    nightmode: bool
+    chatbot_ids: list
+
+
+# Start with temporary values ...
+Config.mute = False
+Config.last = dict()
+Config.voter = list()
+Config.voters = list()
+Config.nightmode = False
+Config.muted_ids = list()
+Config.chatbot_ids = list()
