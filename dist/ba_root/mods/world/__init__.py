2109338d49459239c8994923babcc237c173e56f
LIRIK <74240325+LIRIK-SPENCER@users.noreply.github.com>
2021-08-11 18:43:45 +0530

    v2.2

--- dist/ba_root/mods/world/__init__.py
+++ dist/ba_root/mods/world/__init__.py
@@ -1,7 +1,7 @@
 # STANDARD MODS LIBRARY FOR EXTRA MODS
 
 
-__version__ = 2.0
+__version__ = 2.2
 __author__ = "LIRIK SPENCER"
 
 # I will update this as i get ideas from you guys !
--- dist/ba_root/mods/world/system/__init__.py
+++ dist/ba_root/mods/world/system/__init__.py
@@ -7,6 +7,6 @@ from . import (
     internal,
     session,
     mapdata,
-    decorate,
+    spaz,
     mode,
 )
