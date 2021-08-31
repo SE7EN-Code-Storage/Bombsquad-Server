894997cc2909ef365dd113771aca8f0d67eeec74
LIRIK <74240325+LIRIK-SPENCER@users.noreply.github.com>
2021-08-31 19:01:33 +0530

    v4.1

--- dist/ba_root/mods/world/settings.py
+++ dist/ba_root/mods/world/settings.py
@@ -1,39 +1,40 @@
-from tools.configclass import ConfigMaster
+from tools.configclass import configs
+
 
 __all__ = ["configs"]
 
 
-def server_config():
-    """
-    # The Main Thing ...
-    >>> from settings import configs
+"""
+# The Main Thing ...
+>>> from settings import configs
 
-    # Access config members like its a class attribute
-    >>> configs.auto_role.status
-    True
+# Access config members like its a class attribute
+>>> configs.auto_role.status
+True
 
-    # Get List of all Sections
-    >>> configs.sections
-    ["chat", "nightmode", "autorole", "cooldown", "backup_service", "game"]
+# Get List of all Sections
+>>> configs.sections
+["chat", "nightmode", "autorole", "cooldown", "backup_service", "game"]
 
-    # Change values just like that
-    >>> configs.auto_role.status = False
-    >>> configs.auto_role.status
-    False
+# Change values just like that
+>>> configs.auto_role.status = False
+>>> configs.auto_role.status
+False
 
-    # Adding more sections
-    >>> configs.add_section("new_section")
-    # Even with increment
-    >>> configs += {"new_section": {"settings": True}}
+# Adding more sections
+>>> configs.add_section("new_section")
+#--- Even with increment
+>>> configs += {"new_section": {"settings": True}}
 
-    # Add more options to sections
-    >>> configs.new_section.add_option("status", True)
-    # Even with increment
-    >>> configs.new_section += {"status": True}
-    >>> configs.new_section.status
-    True
-    """
-    return ConfigMaster()
+# Add more options to sections
+>>> configs.new_section.add_option("status", True)
+# Even with increment
+>>> configs.new_section += {"status": True}
+>>> configs.new_section.status
+True
 
+#NOTE: Do not create another instance, as Config class should be initialise at the Runtime
+"""
 
-configs = server_config()
+# This do the initial job of finding and loading config files
+configs.initialise()
